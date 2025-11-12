913579e2d74f37c71a1889099cd03fa592fefd0e
Marian Noekel <mariannoekel@gmail.com>
2025-11-13 00:22:17 +0100

    change build dir to docs

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/susanne-h/', 
   plugins: [react(), tailwindcss()],
+
+// Ausgabeordner auf /docs setzen**
+  build: {
+    outDir: 'docs',
+  },
 });
