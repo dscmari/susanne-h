eb495b0da7cbd0d8d2b52b06e489c12e37ac065a
Marian Noekel <mariannoekel@gmail.com>
2025-11-13 00:07:57 +0100

    adapt vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Wichtig: Der Name deines GitHub-Repositorys ist der Basis-Pfad
-const repoName = 'susanne-h'; 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Füge die 'base'-Eigenschaft hinzu:
-  // Sie sorgt dafür, dass alle Asset-Pfade (CSS, JS, Bilder)
-  // mit '/susanne-h/' beginnen, was den 404-Fehler behebt.
-  base: `/${repoName}/`, 
-  
+  base: '/susanne-h/', 
   plugins: [react(), tailwindcss()],
 });
